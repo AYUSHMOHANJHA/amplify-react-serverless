030b05b070d5caa8a334a57b743fc52788f30385
Ayush <ayush@Ayushs-MacBook-Air.local>
2021-10-08 16:15:24 +0530

    Initial commit - 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi , this is Ayush Jha
+          Hi , this is Ayush Mohan Jha
         </p>
         <a
           className="App-link"
